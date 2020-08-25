c417c3af9a1d0084472445ffdef338db7d9b1a9e
Alok Mishra <hayalok@gmail.com>
2020-08-25 18:38:21 +0530

    final commit

---
+++ app.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var book = require('./routes/book');
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/book', book);
+
+
+module.exports = app;
